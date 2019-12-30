4e6e63cd91aa3598b87d02cd89a3bb47f5268b5b
Ondřej Šibrava <ondrej.sibrava@varhall.cz>
2019-12-30 19:14:12 +0100

    AuthorizationPlugin object validation

--- src/presenters/plugins/AuthorizationPlugin.php
+++ src/presenters/plugins/AuthorizationPlugin.php
@@ -17,8 +17,12 @@ class AuthorizationPlugin extends Plugin
     protected function handle(RestRequest $request, $user)
     {
         $class = $this->presenterCall($request->getPresenter(), 'modelClass');
+        $object = !!$request->id ? $class::find($request->id) : $class;
 
-        if ($user->cant($request->method, !!$request->id ? $class::find($request->id) : $class))
+        if (!!$request->id && !$object)
+            return $this->terminate('Object not found', Response::S404_NOT_FOUND);
+
+        if ($user->cant($request->method, $object))
             return $this->terminate('Operation is not allowed', Response::S403_FORBIDDEN);
 
         return $request->next();
